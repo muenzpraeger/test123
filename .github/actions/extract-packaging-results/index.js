173ea66237ab4f160e388583baed8084c60214a3
muenzpraeger <mail@winkelmeyer.com>
2019-11-29 09:05:37 +0100

    Test2

--- .github/actions/extract-packaging-results/index.js
+++ .github/actions/extract-packaging-results/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 async function run() {
     try {
-        console.log(core.getInput('json'))
+        console.log(core.getInput('json'));
         const jsonResponse = core.getInput('json');
         core.info('Status is: ' + jsonResponse);
         core.setOutput('status', jsonResponse.status === 0 ? true : false);
